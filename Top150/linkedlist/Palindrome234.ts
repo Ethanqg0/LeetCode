783526542712d4909a9fd058ce20e2fb44a10fcf
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2025-01-15 15:01:22 -0800

    Palindrome Linked List

--- Top150/linkedlist/Palindrome234.ts
+++ Top150/linkedlist/Palindrome234.ts
@@ -1,31 +1,69 @@
 /*
-    Method: Reverse the second half of the linked list, and then go by one and by one, as soon as one does not equal the
-    other, we return false
+    Pseudocode:
 
-    Edge Case: Odd amount of elements, no elements
-        Odd: Ignore the middle element, reverse the rest
+    Edge Case:
+        No elements
+        One element
+    
+    Find the middle point using slow and fast pointers
+        Slow pointer moves one spot at a time
+        Fast pointer moves two spots at a time
 
+    Reverse the second half using a standard reversal algorithm for LL
 
-    Psuedocode
-        Edge Case: if no elements, return true
-        Edge Case: if one element, return true
+    Use a while loop to compare the element from each half
+        if they do not equal each other
+            return false
 
-        variable nextNode: Node = head
-        variable middle: Node; 
-
-        for (let i = 0; i < Math.ceiling(length / 2); i++) {
-            nextNode = nextNode->next
-        }
-
-        middle = nextNode
-
-        while (!middle->next) {
-            reverse operation
-        }
-
-        compare first half to second half
-            if first half at index !== second half at index
-                return false
-
-        return true
+    return true
 */
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+function isPalindromeLL(head: ListNode | null): boolean {
+  // 1. Find the middle (slow and fast pointer)
+  if (head === null || head.next === null) {
+    return true;
+  }
+
+  let middlePoint: ListNode | null = null;
+  let slowPointer: ListNode | null = head;
+  let fastPointer: ListNode | null = head;
+
+  while (fastPointer && fastPointer.next) {
+    slowPointer = slowPointer.next;
+    fastPointer = fastPointer.next.next;
+  }
+
+  middlePoint = slowPointer;
+
+  // 2. Reverse the second half (traditional reverse method)
+  let prev: ListNode | null = null;
+  let curr: ListNode | null = middlePoint;
+  let next: ListNode | null = null;
+
+  while (curr !== null) {
+    next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  // 3. Comparing the first half to the second half (while loop with if statements)
+  while (prev) {
+    if (head.val === prev.val) {
+      head = head.next;
+      prev = prev.next;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}

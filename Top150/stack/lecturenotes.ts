5631e308c4c1d5ca4f7a6ece1afce7290e350bdf
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2025-02-04 13:11:21 -0800

    Arrays vs Linked lists for stacks

--- Top150/stack/lecturenotes.ts
+++ Top150/stack/lecturenotes.ts
@@ -1,7 +1,16 @@
 /*
-    Recursion vs Tail Recursion
-    Recursion is notorious for calling stack overflow because of the large amount of stored execution calls on the stack
-    Tail Recursion avoids this by reusing the stack frame of the current function call for the next function call
+    Stack Lecture Notes
+
+    Uses LIFO (Last In First Out) data structure
+
+    You can use an array or a linked list
+        If you are implementing a pure stack (where you only access the first item), then you have O(1)
+        regardless of whether you use an array or a linked list. However, with arrays, you may have to resize the data structure 
+        which is O(n). You can reduce resizing by allocating extra space during initialization.
+
+        Linked lists avoid that problem, but they have extra memory overhead for the pointers 
+        and they are also harder to implement.
+
+        They have their pros and cons, but in general, most people use arrays for stacks.
 */
 
-So 
